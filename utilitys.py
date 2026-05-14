38602ba1fd66e77236afd570665021b8101701b5
wanaw1766 <fozilove1999@gmail.com>
2026-05-14 16:35:38 +0300

    Update utilitys.py

--- utilitys.py
+++ utilitys.py
@@ -2,13 +2,12 @@ from configparser import ConfigParser
 from threading import active_count
 from time import sleep as swait
 from os import system, name
-from tg_views import Api
+from tg_views import Api   # <-- changed from 'telegram'
 from re import search
 from sys import exit
 
-
 THREADS = 400
-LOGO = '''
+LOGO = r'''
 __     ___                     _____    _
 \ \   / (_) _____      _____  |_   _|__| | ___  __ _ _ __ __ _ _ __ ___
  \ \ / /| |/ _ \ \ /\ / / __|   | |/ _ \ |/ _ \/ _` | '__/ _` | '_ ` _ \ _____
@@ -23,32 +22,29 @@ __     ___                     _____    _
 error_file = open('errors.txt', 'a+', encoding='utf-8')
 logger = lambda error: error_file.write(f'{error}\n')
 
-
 def config_loader():
-    try: 
+    try:
         cfg = ConfigParser(interpolation=None)
         cfg.read("config.ini", encoding="utf-8")
         return (
-            cfg["HTTP"].get("Sources").splitlines(), 
-            cfg["SOCKS4"].get("Sources").splitlines(), 
+            cfg["HTTP"].get("Sources").splitlines(),
+            cfg["SOCKS4"].get("Sources").splitlines(),
             cfg["SOCKS5"].get("Sources").splitlines()
         )
-    except KeyError: 
+    except KeyError:
         print(' [ Error ] config.ini not found!')
         swait(3)
         exit()
 
-
 def input_loader():
     url_input = search(r'(https?:\/\/t\.me\/)?([^/]+)/(\d+)', input(' [ INPUT ] Enter Post URL: '))
-    if url_input: 
+    if url_input:
         _, channel, post = url_input.groups()
         return channel, post
-    else: 
+    else:
         print(' [ ERROR ] Channel Or Post Not Found!')
         swait(3)
         exit()
-
 
 def display():
     print(' [ OUTPUT ] Started ( Wait few seconds to run threads )');swait(7)
@@ -67,4 +63,3 @@ def display():
         ''')
     
     return inner
-
