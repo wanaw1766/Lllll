276574a74572067181c5b7f5af7726f3bb32470d
wanaw1766 <fozilove1999@gmail.com>
2026-05-14 10:47:07 +0300

    Update utilitys.py

--- utilitys.py
+++ utilitys.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 from threading import active_count
 from time import sleep as swait
 from os import system, name
-from telegram import Api
+from tg_api import Api   # Changed from telegram import Api
 from re import search
 from sys import exit
 
